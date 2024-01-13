64fba240e151f6c92e8d2abbacf9a7745077bb3a
Nicholas Christensen <nickachristensen@gmail.com>
2024-01-12 22:43:36 -0500

    Add sphere and plane to Experience.jsx

--- lessons/37-first-rf3-application/src/Experience.jsx
+++ lessons/37-first-rf3-application/src/Experience.jsx
@@ -1,9 +1,24 @@
+import { useFrame } from '@react-three/fiber'
+
 export default function Experience() {
+
+    useFrame(() => {
+        console.log('tick')
+    })
+    
     return (
         <>
+            <mesh position-x={ -2 }>
+                <sphereGeometry />
+                <meshBasicMaterial color="orange" />
+            </mesh>
             <mesh rotation-y={ Math.PI * 0.23 } position-x={ 3 } scale={ 1.5 }>
                 <boxGeometry scale= { 1.5 } />
-                <meshBasicMaterial color="mediumpurple" wireframe />
+                <meshBasicMaterial color="mediumpurple" />
+            </mesh>
+            <mesh position-y={ -1 } rotation-x={ -Math.PI * 0.5 } scale={ 10 }>
+                <planeGeometry />
+                <meshBasicMaterial color="greenyellow" />
             </mesh>
         </>     
     )
