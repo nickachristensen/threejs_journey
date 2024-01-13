d1e75a9eee9e5a437c604546c657e42b0caba1fe
Nicholas Christensen <nickachristensen@gmail.com>
2024-01-12 22:50:19 -0500

    Add rotation to cube in Experience.jsx

--- lessons/37-first-rf3-application/src/Experience.jsx
+++ lessons/37-first-rf3-application/src/Experience.jsx
@@ -1,25 +1,31 @@
 import { useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
 
 export default function Experience() {
+    const cubeRef = useRef()
 
-    useFrame(() => {
-        console.log('tick')
+    useFrame((state, delta) => {
+        cubeRef.current.rotation.y += delta
     })
-    
+
     return (
         <>
-            <mesh position-x={ -2 }>
-                <sphereGeometry />
-                <meshBasicMaterial color="orange" />
-            </mesh>
-            <mesh rotation-y={ Math.PI * 0.23 } position-x={ 3 } scale={ 1.5 }>
-                <boxGeometry scale= { 1.5 } />
-                <meshBasicMaterial color="mediumpurple" />
-            </mesh>
-            <mesh position-y={ -1 } rotation-x={ -Math.PI * 0.5 } scale={ 10 }>
-                <planeGeometry />
-                <meshBasicMaterial color="greenyellow" />
-            </mesh>
+            <group>
+                <mesh position-x={ -2 }>
+                    <sphereGeometry />
+                    <meshBasicMaterial color="orange" />
+                </mesh>
+
+                <mesh ref={ cubeRef } rotation-y={ Math.PI * 0.23 } position-x={ 3 } scale={ 1.5 }>
+                    <boxGeometry scale= { 1.5 } />
+                    <meshBasicMaterial color="mediumpurple" />
+                </mesh>
+            </group>
+
+                <mesh position-y={ -1 } rotation-x={ -Math.PI * 0.5 } scale={ 10 }>
+                    <planeGeometry />
+                    <meshBasicMaterial color="greenyellow" />
+                </mesh>
         </>     
     )
 }
