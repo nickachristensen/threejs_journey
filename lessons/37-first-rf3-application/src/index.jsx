29a16fe586a50d5856eddf525fe2240f700f0e73
Nicholas Christensen <nickachristensen@gmail.com>
2024-01-19 23:46:26 -0500

    Finish R3f lesson

--- lessons/37-first-rf3-application/src/index.jsx
+++ lessons/37-first-rf3-application/src/index.jsx
@@ -2,11 +2,28 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
+import * as THREE from 'three'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-        <Canvas>
+        <Canvas
+            dpr={ [ 1, 2 ]}
+            // flat
+            gl={ {
+                antialias: true,
+                toneMapping: THREE.ACESFilmicToneMapping,
+                outputColorSpace: THREE.SRGBColorSpace
+            } }
+            // orthographic
+            camera={ {
+                fov: 45,
+                // zoom: 100,
+                near: 0.1,
+                far: 200,
+                position: [ 3, 2, 6 ]
+            } }
+        >
             <Experience />
         </Canvas>
 )
